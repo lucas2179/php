6009ba0bbd4fe5438041b63ef9c99d4422bee5b9
Lucas Aparecido Dantas Do Nascimento de Souza <lucas.aparecido.souza@ibm.com>
2019-10-10 17:51:31 -0300

    foi

--- index.php
+++ index.php
@@ -1,15 +1,51 @@
-<?php
-echo "<h2>Cadastro do Evento</h2>";
-echo "Digite seu ID da IBM Cloud: <br>";
-echo "<form action=\"index.php\" method=\"post\">";
-echo "<input type=\"text\" name=\"ent1\">";
-echo "<input type=\"submit\">";
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Cadastro Lab K8S</title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+        <link rel="stylesheet" href="form.css" >
+        <script src="form.js"></script>
+    </head>
+    <body >
+        <div class="container">
+            <div id="form-main">
+                <div id="form-div">
+                    <form class="montform" id="reused_form" action = "index.php" >
+                        
+                        <p class="email">
+                            <input name="ent1" type="text" required class="feedback-input" id="email" placeholder="Email" />
+                        </p>
+                        
+                        <div class="submit">
+                            <button type="submit" class="button-blue">SUBMIT</button>
+                            <div class="ease"></div>
+                        </div>
+                    </form>
+                    <div id="error_message" style="width:100%; height:100%; display:none; ">
+                        <h4>
+                            Error
+                        </h4>
+                        Sorry there was an error sending your form. 
+                    </div>
+                    <div id="success_message" style="width:100%; height:100%; display:none; "> <h2>Success! Your Message was Sent Successfully.</h2> </div>
+                </div>
+            </div>
+        </div>
+    <?php
+    	if(isset($_POST["ent1"])){
+    		$queue = $_POST["ent1"];
+    		$fh = fopen("temp.txt", 'a');
+    		fwrite($fh, $queue);
+    		fwrite($fh, "\r\n");
+    		fclose($fh);
+		}
+	?>
 
-if(isset($_POST["ent1"])){
-    $queue = $_POST["ent1"];
-    $fh = fopen("temp.txt", 'a');
-    fwrite($fh, $queue);
-    fwrite($fh, "\r\n");
-    fclose($fh);
-}
-?>
+    ?>
+    </body>
+
+</html>
