db398d4208f1cd5c2e3533ed559d0e267aebbed4
Lucas Souza <42921327+lucas2179@users.noreply.github.com>
2019-10-10 18:27:40 -0300

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
                     <form class="montform" id="reused_form">
                         
                         <p class="email">
-                            <input name="email" type="text" required class="feedback-input" id="email" placeholder="Email" action="handler.php" />
+                            <input name="email" type="text" required class="feedback-input" id="email" placeholder="Email" action="handler.php" method="post" />
                         </p>
                         
                         <div class="submit">
